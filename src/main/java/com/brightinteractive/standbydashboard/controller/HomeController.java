ef9e923d31bf81d49a75960349fb4fb6562cf06d
wheelr <nickw@bright-interactive.co.uk>
2014-10-15 13:15:27 +0100

    Restrict number of lines of log to show.

--- src/main/java/com/brightinteractive/standbydashboard/controller/HomeController.java
+++ src/main/java/com/brightinteractive/standbydashboard/controller/HomeController.java
@@ -1,9 +1,10 @@
 package com.brightinteractive.standbydashboard.controller;
 
-import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import net.redhogs.cronparser.CronExpressionDescriptor;
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 @Controller
 public class HomeController {
@@ -22,6 +23,7 @@ public class HomeController {
 	private String source;
 	private String destination;
 	private String schedule;
+	private final int LOG_LINES_TO_SHOW = 6;
 
 	@Value("${source.directory}")
 	public void setSource(String source)
@@ -51,7 +53,13 @@ public class HomeController {
 	public ModelAndView test(HttpServletResponse response) throws IOException
 	{
 		Map model = new HashMap();
-		model.put("log", FileUtils.readFileToString(new File("sync.log")));
+		List<String> logLines = IOUtils.readLines(new FileInputStream("sync.log"), "UTF-8");
+
+		if (logLines.size() > LOG_LINES_TO_SHOW)
+		{
+			logLines = logLines.subList(logLines.size() - LOG_LINES_TO_SHOW, logLines.size());
+		}
+		model.put("logLines", logLines);
 		model.put("source", source);
 		model.put("destination", destination);
 		model.put("schedule", schedule);
